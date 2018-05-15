62e7b31cc96b799e2f855effb87f5901216b9972
Julianny Favinha <julianny.favinha@gmail.com>
2018-05-15 14:13:14 -0300

    Table view (main) with headers and rows. Multiple selection of rows (with checkmark)

--- PhotoWall/Presentation/Albums/Main Split/AlbumsTableViewController.swift
+++ PhotoWall/Presentation/Albums/Main Split/AlbumsTableViewController.swift
@@ -13,11 +13,17 @@ class AlbumsTableViewController: UIViewController {
     //var detailViewController: 
     var splitRootViewController: AlbumsSplitViewController?
     
-    // Rows
-    let rowName: [String] = ["Facebook"]
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Headers and rows
+    let headers: [String] = ["Local", "Facebook", "Instagram"]
+    let rows: [[String]] = [["City", "Default", "Nature"], ["Feed", "Album 1", "Album 2"], ["Your photos"]]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // allows selecing multiple albums
+        self.tableView.allowsMultipleSelection = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,27 +35,52 @@ extension AlbumsTableViewController: UITableViewDataSource, UITableViewDelegate 
     
     // Number displayed Cells
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rowName.count
+        return self.rows[section].count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Albums"
+        return self.headers[section]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.headers.count
     }
     
     /// Create table view cells
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell?.textLabel?.text = rowName[indexPath.row]
+        cell?.textLabel?.text = self.rows[indexPath.section][indexPath.row]
         return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didUpdateFocusIn context: UITableViewFocusUpdateContext,
+                   with coordinator: UIFocusAnimationCoordinator) {
+//        self.splitRootViewController?.facebookViewController?.label.text = self.rows[(context.nextFocusedIndexPath?.section)!][(context.nextFocusedIndexPath?.row)!]
     }
     
     /// Change Detail view
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.row == 0 {
-            //Album Facebook detail view
-            self.splitRootViewController?.showDetailViewController(
-                (splitRootViewController?.facebookViewController)!, sender: self)
+//        if indexPath.row == 0 {
+//            //Album Facebook detail view
+//            self.splitRootViewController?.showDetailViewController(
+//                (splitRootViewController?.facebookViewController)!, sender: self)
+//        }
+        if let cell = self.tableView.cellForRow(at: indexPath) {
+            checkCell(cell: cell, indexPath: indexPath)
         }
     }
     
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = self.tableView.cellForRow(at: indexPath) {
+            checkCell(cell: cell, indexPath: indexPath)
+        }
+    }
+    
+    private func checkCell(cell: UITableViewCell, indexPath: IndexPath) {
+        if cell.accessoryType == .checkmark {
+            cell.accessoryType = .none
+        } else {
+            cell.accessoryType = .checkmark
+        }
+    }
 }
