113d8cebd15c86addc4782b6605d5db0d27ddb11
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-05-22 11:00:48 -0300

    Changed theme images

--- PhotoWall/Presentation/Themes/ThemeCollectionViewCell.swift
+++ PhotoWall/Presentation/Themes/ThemeCollectionViewCell.swift
@@ -13,8 +13,26 @@ class ThemeCollectionViewCell: ImageCollectionViewCell {
     
     override func didMoveToSuperview() {
         super.didMoveToSuperview()
-        self.layer.cornerRadius = 20
-        self.clipsToBounds = true
         self.imageView.contentMode = .scaleAspectFill
+        self.imageView.clipsToBounds = true
+    }
+    
+    //Animate views according to focus engine changes
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        if let cell = context.nextFocusedView as? UICollectionViewCell {
+            UIView.animate(withDuration: 0.3, animations: {
+                cell.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+                self.layer.shadowOpacity = 0.5
+                self.layer.shadowRadius = 10
+            }, completion: nil)
+        }
+        
+        if let cell = context.previouslyFocusedView as? UICollectionViewCell {
+            UIView.animate(withDuration: 0.3, animations: {
+                cell.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                self.layer.shadowOpacity = 0.3
+                self.layer.shadowRadius = 5
+            }, completion: nil)
+        }
     }
 }
