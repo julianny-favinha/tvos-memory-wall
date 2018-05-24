a3cc720e6d7228034bc53cc62fd2c811342dd4a2
Julianny Favinha <julianny.favinha@gmail.com>
2018-05-24 11:58:28 -0300

    Number of executions was changed to 1 (1st time opening the app)

--- PhotoWall/Presentation/PhotoWall/PhotoWallViewController.swift
+++ PhotoWall/Presentation/PhotoWall/PhotoWallViewController.swift
@@ -59,7 +59,7 @@ class PhotoWallViewController: UIViewController {
         self.assistantView.alpha = 0
         
         // First time executing the app
-        if UserDefaultsManager.getNumberOfExecutions() == 0 {
+        if UserDefaultsManager.getNumberOfExecutions() == 1 {
             self.displayMessage("Welcome to PhotoWall! Press the Play/Pause Button to start animating your wall. \n" +
                 "Go to \"Settings\" to log into your accounts or to \"Albums\" to change the displayed photos.")
         }
