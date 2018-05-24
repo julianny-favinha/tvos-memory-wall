f5b0f1978a1a91c8815e8d5bd92378314369a5e8
Julianny Favinha <julianny.favinha@gmail.com>
2018-05-24 09:54:44 -0300

    Top shelf images and cork background

--- PhotoWall/Model/ThemeCustomization.swift
+++ PhotoWall/Model/ThemeCustomization.swift
@@ -18,6 +18,7 @@ enum Backgrounds: String {
     case darkGray = "Dark Gray"
     case beach = "Beach"
     case woodWall = "Wood Wall"
+    case cork = "Cork"
 }
 
 enum Layouts: String {
@@ -34,56 +35,85 @@ enum Cells: String {
 
 
 class ThemeCustomization {
-    
-    
-    // Backgrounds
+    // MARK: Backgrounds
     static let backgrounds: [Backgrounds] =
-        [.light, .lightPink, .lightBlue, .confetti, .dark, .darkGray, .beach, .woodWall]
+        [.light,
+         .lightPink,
+         .lightBlue,
+         .confetti,
+         .dark,
+         .darkGray,
+         .beach,
+         .woodWall,
+         .cork]
     
     static let backgroundImage: [Backgrounds: UIImage] =
-        [.light: #imageLiteral(resourceName: "light"), .lightPink: #imageLiteral(resourceName: "lightPink"), .lightBlue: #imageLiteral(resourceName: "lightBlue"), .confetti: #imageLiteral(resourceName: "confetti2"), .dark: #imageLiteral(resourceName: "dark"), .darkGray: #imageLiteral(resourceName: "darkGray"),
-         .beach: #imageLiteral(resourceName: "beach.gif"), .woodWall: #imageLiteral(resourceName: "woodWall")]
+        [.light: #imageLiteral(resourceName: "light"),
+         .lightPink: #imageLiteral(resourceName: "lightPink"),
+         .lightBlue: #imageLiteral(resourceName: "lightBlue"),
+         .confetti: #imageLiteral(resourceName: "confetti2"),
+         .dark: #imageLiteral(resourceName: "dark"),
+         .darkGray: #imageLiteral(resourceName: "darkGray"),
+         .beach: #imageLiteral(resourceName: "beach.gif"),
+         .woodWall: #imageLiteral(resourceName: "woodWall"),
+         .cork: #imageLiteral(resourceName: "cork")]
     
     static let backgroundColors: [Backgrounds: UIColor] =
-        [.light: .clear, .lightPink: #colorLiteral(red: 1, green: 0.5411764706, blue: 0.8470588235, alpha: 0.1993792808), .lightBlue: #colorLiteral(red: 0.5098039216, green: 0.8549019608, blue: 0.9607843137, alpha: 0.4952108305), .confetti: .clear,
-         .dark: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).withAlphaComponent(0.7), .darkGray: #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), .beach: .clear, .woodWall: .clear]
+        [.light: .clear,
+         .lightPink: #colorLiteral(red: 1, green: 0.5411764706, blue: 0.8470588235, alpha: 0.1993792808),
+         .lightBlue: #colorLiteral(red: 0.5098039216, green: 0.8549019608, blue: 0.9607843137, alpha: 0.4952108305),
+         .confetti: .clear,
+         .dark: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).withAlphaComponent(0.7),
+         .darkGray: #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1),
+         .beach: .clear,
+         .woodWall: .clear,
+         .cork: .clear]
     
     static let backgroundViews: [Backgrounds: UIView?] =
-        [.light: nil, .lightPink: nil, .lightBlue: nil,
-         .dark: nil, .darkGray: nil, .confetti: ConfettiView(),
-         .beach: GIFBackground(image: "beach", ext: "gif"), .woodWall: BackgroundImageView(image: #imageLiteral(resourceName: "woodWall"))]
+        [.light: nil,
+         .lightPink: nil,
+         .lightBlue: nil,
+         .dark: nil,
+         .darkGray: nil,
+         .confetti: ConfettiView(),
+         .beach: GIFBackground(image: "beach", ext: "gif"),
+         .woodWall: BackgroundImageView(image: #imageLiteral(resourceName: "woodWall")),
+         .cork: BackgroundImageView(image: #imageLiteral(resourceName: "cork"))]
     
-    
-    
-    
-    // Layouts
+    // MARK: Layouts
     static let layouts: [Layouts] =
-        [.singleLine, .twoLines, .singleImage]
+        [.singleLine,
+         .twoLines,
+         .singleImage]
     
     static let layoutImages: [Layouts: UIImage] =
-        [.singleLine: #imageLiteral(resourceName: "oneLine"), .twoLines: #imageLiteral(resourceName: "twoLines"), .singleImage: #imageLiteral(resourceName: "singleImage")]
+        [.singleLine: #imageLiteral(resourceName: "oneLine"),
+         .twoLines: #imageLiteral(resourceName: "twoLines"),
+         .singleImage: #imageLiteral(resourceName: "singleImage")]
     
     static let layoutLayouts: [Layouts: CustomLayout] =
-        [.singleLine: DefaultLayout(), .twoLines: LinedGridLayout(numberOfLines: 2),
+        [.singleLine: DefaultLayout(),
+         .twoLines: LinedGridLayout(numberOfLines: 2),
          .singleImage: FullScreenLayout()]
     
-    
-    
-    
-    // Cells
+    // MARK: Cells
     static let cells: [Cells] =
-        [.simple, .polaroid, .blackWhite]
+        [.simple,
+         .polaroid,
+         .blackWhite]
     
     static let cellsImages: [Cells: UIImage] =
-        [.simple: #imageLiteral(resourceName: "whiteThemeImage"), .polaroid: #imageLiteral(resourceName: "pinThemeImage"), .blackWhite: #imageLiteral(resourceName: "pbThemeImage")]
+        [.simple: #imageLiteral(resourceName: "whiteThemeImage"),
+         .polaroid: #imageLiteral(resourceName: "pinThemeImage"),
+         .blackWhite: #imageLiteral(resourceName: "pbThemeImage")]
     
     static let cellProcessor: [Cells: ImageProcessor] =
-        [.simple: DefaultImageProcessor(), .polaroid: DefaultImageProcessor(),
+        [.simple: DefaultImageProcessor(),
+         .polaroid: DefaultImageProcessor(),
          .blackWhite: BlackWhiteProcessor()]
     
     static let cellIdentifier: [Cells: String] =
         [.simple: "ImageCollectionViewCell",
          .polaroid: "PolaroidCollectionViewCell",
          .blackWhite: "ImageCollectionViewCell"]
-    
 }
