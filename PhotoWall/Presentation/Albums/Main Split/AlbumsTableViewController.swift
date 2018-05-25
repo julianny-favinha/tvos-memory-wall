3d8372b0ef83a236a21b8c0d4ecd502908b0e632
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-05-25 15:01:47 -0300

    Removed unused prints

--- PhotoWall/Presentation/Albums/Main Split/AlbumsTableViewController.swift
+++ PhotoWall/Presentation/Albums/Main Split/AlbumsTableViewController.swift
@@ -179,10 +179,8 @@ extension AlbumsTableViewController: UITableViewDataSource, UITableViewDelegate 
     
     private func checkCell(cell: UITableViewCell, indexPath: IndexPath) {
         if cell.accessoryType == .checkmark {
-            print("out")
             cell.accessoryType = .none
         } else {
-            print("in")
             cell.accessoryType = .checkmark
         }
         
